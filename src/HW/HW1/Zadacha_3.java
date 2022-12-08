2be4e0c53cc2a965eaa82e7b2dec470274c767b9
Natalya <ivanechka93@mail.ru>
2022-12-08 21:59:29 +0300

    3. Метод, принимающий два целочисленных массива, и возвращающий новый массив, каждый элемент которого равен разности элементов двух входящих массивов в той же ячейке (с проверкой на null и на длину)

--- src/HW/HW1/Zadacha_3.java
+++ src/HW/HW1/Zadacha_3.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 
 public class Zadacha_3 {
     public static void main(String[] args) {
-        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array1 = {1, 2, 4, 5};
 //      int[] array1 = null;
         int[] array2 = {0, 2, 3, 4, 6};
         System.out.println(Arrays.toString(arrNew(array1, array2)));
@@ -18,11 +18,11 @@ public class Zadacha_3 {
     public static int[] arrNew(int[] array1, int[] array2) {
 
         if (array1 == null || array2 == null) { // проверка на null
-            throw new NullPointerException ("Массив не может быть NULL!");
+            throw new RuntimeException ("Массив не может быть NULL!");
         }
 
         if (array1.length != array2.length) { // проверка на длину массива
-            throw new ArrayIndexOutOfBoundsException ("Массивы должны быть равной длины!");
+            throw new RuntimeException ("Массивы должны быть равной длины!");
         }
 
         int[] newArray = new int[array1.length];
