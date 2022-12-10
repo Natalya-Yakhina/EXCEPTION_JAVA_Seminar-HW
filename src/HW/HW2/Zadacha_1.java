a40e97663c6f6a025714fdb1c477fc6156cf699e
Natalya <ivanechka93@mail.ru>
2022-12-10 19:59:55 +0300

    метод, с вводом дробного числа, с повторным вводом данных

--- src/HW/HW2/Zadacha_1.java
+++ src/HW/HW2/Zadacha_1.java
@@ -4,5 +4,25 @@ package HW.HW2;
 // и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
 // вместо этого, необходимо повторно запросить у пользователя ввод данных.
 // В этом задании не используем try catch. Пишем регулярное выражение и используем на строке метод matches.
+
+import java.util.Scanner;
+
 public class Zadacha_1 {
+    static Scanner iScanner = new Scanner(System.in, "ibm866");
+    static String input;
+    public static void main(String[] args) {
+        System.out.println(inpFloat());
+    }
+    public static float inpFloat() {
+
+        System.out.println("Введите число типа float c точкой: ");
+        input = iScanner.next();
+
+        while (!input.matches("[+-]?([0-9]*[.])?[0-9]+")) { // если значение введено не верно, запросит ввод еще раз, до тех пор пока не будет введено число
+            System.out.println("Введите число используя точку! Неверный ввод!");
+            input = iScanner.nextLine();
+        }
+        iScanner.close();
+        return Float.parseFloat(input); // parseFloat - возвращает число с плавающей запятой
+    }
 }
