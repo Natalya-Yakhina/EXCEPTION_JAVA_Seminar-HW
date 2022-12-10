0974d5fda9563c557b5c06afa88a7ceedd20fd31
Natalya <ivanechka93@mail.ru>
2022-12-10 20:34:34 +0300

    исправить код

--- src/HW/HW2/Zadacha_2.java
+++ src/HW/HW2/Zadacha_2.java
@@ -1,15 +1,42 @@
 package HW.HW2;
 
 // 2. Если необходимо, исправьте данный код
-//try {
+//  try {
 //        int d = 0;
 //        double catchedRes1 = intArray[8] / d;
 //        System.out.println("catchedRes1 = " + catchedRes1);
-//        } catch (ArithmeticException e) {
+//  } catch (ArithmeticException e) {
 //        System.out.println("Catching exception: " + e);
-//        }
-
-или через if, или добавить еше одну обработку
+//  }
 
 public class Zadacha_2 {
+    public static void main(String[] args) {
+
+        int[] intArray = new int[8];
+        int index = 8;
+        int d = 0;
+
+        if (d == 0) {
+            throw new ArithmeticException("Делить на ноль нельзя!");
+        }
+        if (index > intArray.length) {
+            throw new ArrayIndexOutOfBoundsException("Индекс выходит за пределы массива");
+        }
+
+        double catchedRes1 = intArray[index] / d;
+        System.out.println("catchedRes1 = " + catchedRes1);
+    }
 }
+
+// добавила в try - catch, при котором индекс может быть за пределами массива + еxception (c инициализацией?)
+//        try {
+//            double catchedRes1 = intArray[8] / d;
+//            System.out.println("catchedRes1 = " + catchedRes1);
+//        } catch (ArithmeticException e) {
+//            System.out.println("Деление на ноль недопустимо: " + e);
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Индекс за пределами массива" + e);
+//        } catch (Exception e){
+//            System.out.println("Catching exception: " + e.getClass().getSimpleName());
+//        }
+
