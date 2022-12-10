675a164b9add2acb888561136da3c334d10171a7
Natalya <ivanechka93@mail.ru>
2022-12-10 14:55:04 +0300

    HTTP

--- src/Seminar2/ClientService.java
+++ src/Seminar2/ClientService.java
@@ -1,28 +1,28 @@
-package Seminar2;
-
-public class ClientService {
-    HttpClient client = HttpClient.newBuilder().build();
-
-    public String get(String sourceUrl) throws URISyntaxException, IOException, InterruptedException {
-        HttpRequest httpRequest = HttpRequest // строим запрос
-                .newBuilder()
-                .url(new URI(sourceUrl))
-                .GET()
-                .build();
-//        String filename = sourceUrl.substring(sourceUrl.lastIndexOf('/') + 1)
-
-        HttpResponse<String> response;
-        try {
-            response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        } catch (Exception e) {
-
-        }
-
-        if (response.statusCode() != 200) {
-            throw new RuntimeException("Ошибка при скачивании");
-        }
-
-        System.out.println(response.body());
-        return response.body();
-    }
-}
+//package Seminar2;
+//
+//public class ClientService {
+//    HttpClient client = HttpClient.newBuilder().build();
+//
+//    public String get(String sourceUrl) throws URISyntaxException, IOException, InterruptedException {
+//        HttpRequest httpRequest = HttpRequest // строим запрос
+//                .newBuilder()
+//                .url(new URI(sourceUrl))
+//                .GET()
+//                .build();
+////        String filename = sourceUrl.substring(sourceUrl.lastIndexOf('/') + 1)
+//
+//        HttpResponse<String> response;
+//        try {
+//            response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+//        } catch (Exception e) {
+//
+//        }
+//
+//        if (response.statusCode() != 200) {
+//            throw new RuntimeException("Ошибка при скачивании");
+//        }
+//
+//        System.out.println(response.body());
+//        return response.body();
+//    }
+//}
