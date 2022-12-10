0b83e44630714fd315f695b18aa8ea52e5210f27
Natalya <ivanechka93@mail.ru>
2022-12-10 21:13:08 +0300

    программа на ввод строки, при пустой строки - ошибка

--- src/HW/HW2/Zadacha_4.java
+++ src/HW/HW2/Zadacha_4.java
@@ -3,8 +3,17 @@ package HW.HW2;
 // 4. Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
 // Пользователю должно показаться сообщение, что пустые строки вводить нельзя. (try быть не должно)
 
-// через сканер метод некст, если строка пустая то троуэксепшн
-// проверяем на пустоту и все
+import java.util.Scanner;
 
 public class Zadacha_4 {
+    static Scanner iScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Введите строку: ");
+        String input = iScanner.nextLine();
+
+        if(input == ""){
+            throw new RuntimeException("Пустую строку вводить нельзя!");
+        }
+        System.out.println("Введенная вами строка: " + input);
+    }
 }
