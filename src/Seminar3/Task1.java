78a9f03e3b5519e82dcae8403f8f5b5025261c88
Яхина Н А <ivanechka93@mail.ru>
2022-12-15 13:28:18 +0300

    исправить код

---
+++ src/Seminar3/Task1.java
@@ -0,0 +1,40 @@
+package Seminar3;
+
+//public void rwLine(Path pathRead,Path pathWrite)trows IOException{
+//        BufferedReader in=null;
+//        BufferedWriter out=null;
+//        try{
+//            in = Files.newBufferReader(pathRead);
+//            out = Files.newBufferWriter(pathWrite);
+//            out.write(in.readLine());
+//        } finally {
+//            try {
+//                if (in != null) in.close();
+//        } catch(IOException e);
+//        }
+//        try{
+//            if(out != null) out.close();
+//        } catch(IOException e)
+//        }
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+
+//    public void rwLine(Path pathRead, Path pathWrite) {
+//        try (BufferedReader in = Files.newBufferedReader(readPath)) ;
+//        BufferedWriter out = Files.newBufferedWriter(writePath);
+//    } {
+//        out.write(in.readLine());
+//    } catch (IOException e){
+//        System.out.println("Ошибка чтения файла");
+//    }
+//}
